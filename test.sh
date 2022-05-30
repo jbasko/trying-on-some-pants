c14fe68060925abd36ea61c3847ce26dec37d282
Jazeps Basko <jazeps.basko@gmail.com>
2022-05-30 10:01:33 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,10 @@ set -e
 
 ./pants generate-lockfiles --resolve=ide --resolve=python-default
 
-./pants package src/py/exes/example-exe/example_exe:__main__
+./pants package src/py/exes/example-exe/example_exe:bin
 
-./pants run src/py/exes/example-exe/example_exe:__main__
+./pants run src/py/exes/example-exe/example_exe:bin
+
+./pants package src/py/exes/example-exe/Dockerfile
+
+./pants run src/py/exes/example-exe/Dockerfile
